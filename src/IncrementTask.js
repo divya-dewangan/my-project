c0190599f86e900e9da361e6258ff723aaea0eb6
divya-dewangan <divyadewangan114@gmail.com>
2023-07-28 18:15:42 +0530

    Pretify incrrement task

--- src/IncrementTask.js
+++ src/IncrementTask.js
@@ -2,22 +2,22 @@ import React, { useState } from 'react'
 import ViewIncrement from './ChildComponent/ViewIncrement'
 
 function IncrementTask() {
-    const [value,setValue]=useState(0)
-    const clickHandler = () => {
-        setValue(value + 1)
-    }
+  const [value, setValue] = useState(0)
+  const clickHandler = () => {
+    setValue(value + 1)
+  }
 
-    const dicrimentHandler = () => {
-        setValue(value - 1)
-    }
+  const dicrimentHandler = () => {
+    setValue(value - 1)
+  }
 
   return (
     <>
-    <ViewIncrement value={value}/> 
-    <button className='btn btn-info' onClick={clickHandler}>
-    Increment
-    </button>
-    <button className='btn btn-danger mx-2' onClick={dicrimentHandler}>Dicrement</button>
+      <ViewIncrement value={value} />
+      <button className='btn btn-info' onClick={clickHandler}>
+        Increment
+      </button>
+      <button className='btn btn-danger mx-2' onClick={dicrimentHandler}>Dicrement</button>
     </>
   )
 }
